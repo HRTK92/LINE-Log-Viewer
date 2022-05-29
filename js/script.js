97575b45a0ea3327fce0314fc17db6c58d49b6ac
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-05-29 11:03:00 +0000

    通話時間の計測とその他の調整

--- js/script.js
+++ js/script.js
@@ -10,32 +10,30 @@ function loadFileAsText() {
   fileReader.onload = function (fileLoadedEvent) {
     const textFromFileLoaded = fileLoadedEvent.target.result
     window.file_text = textFromFileLoaded
-    GetGata(textFromFileLoaded)
+    GetData(textFromFileLoaded)
   }
 
   fileReader.readAsText(fileToLoad, 'UTF-8')
 }
-function GetGata(got_text) {
+function GetData(got_text) {
   $('#status-text').text('ファイルを分析中')
   bar.value += 20
   const lines = got_text.split('\n')
 
-  let message = 0
-  let text = 0
-  let stamp = 0
-  let image = 0
-  let movie = 0
+  let statistics = { message: 0, text: 0, stamp: 0, image: 0, video: 0, call:0 }
 
   const users = {}
   const message_data = {}
-
   const date = {}
   const diary = {}
+  const call = []
+  let LastStartedCall
+
   let Over_1000_days = 0
   let line_date
   let lastUser
 
-  // 分析
+  /*分析*/
   for (let line = 0; line < lines.length; line++) {
     const regex_date = new RegExp(/^\d\d\d\d[/]\d?\d[/]\d?\d[(].[)]/)
     if (regex_date.test(lines[line])) {
@@ -44,41 +42,59 @@ function GetGata(got_text) {
     }
     const regex = new RegExp(/^\d?\d:\d\d/)
     if (regex.test(lines[line])) {
-      message++
+      statistics['message']++
+
+      const SentTime = lines[line].split(/\s/)[0]
       const name = lines[line].split(/\s/)[1]
+
       message_data[line_date][lines[line].match(regex)[0]] = {
         user: name,
         contents: lines[line],
       }
+
+      let user
       if (users[name] == null) {
         users[name] = {}
-        users[name]['message'] = 0
-        users[name]['mention'] = 0
-        users[name]['continuously'] = []
-        users[name]['join'] = line_date.slice(0, -3)
-        users[name]['contents'] = {}
-        users[name]['contents']['text'] = 0
-        users[name]['contents']['image'] = 0
-        users[name]['contents']['movie'] = 0
-        users[name]['contents']['stamp'] = 0
+        user = users[name]
+        user['message'] = 0
+        user['mention'] = 0
+        user['continuously'] = []
+        user['join'] = line_date.slice(0, -3)
+        user['contents'] = {}
+        user['contents']['text'] = 0
+        user['contents']['image'] = 0
+        user['contents']['movie'] = 0
+        user['contents']['stamp'] = 0
+        user['contents']['call'] = 0
+      } else {
+        user = users[name]
       }
       if (date[line_date] == null) {
         date[line_date] = 0
       }
+
       date[line_date]++
-      users[name]['message']++
+      user['message']++
+
       if (lines[line].indexOf('[写真]') !== -1) {
-        image++
-        users[name]['contents']['image']++
+        statistics['image']++
+        user['contents']['image']++
       } else if (lines[line].indexOf('[動画]') !== -1) {
-        movie++
-        users[name]['contents']['movie']++
+        statistics['video']++
+        user['contents']['movie']++
       } else if (lines[line].indexOf('[スタンプ]') !== -1) {
-        stamp++
-        users[name]['contents']['stamp']++
+        statistics['stamp']++
+        user['contents']['stamp']++
+      } else if (lines[line].indexOf('グループ音声通話が開始されました。') !== -1) {
+        user['contents']['call']++
+        LastStartedCall = line_date.slice(0, -3) + ' ' + SentTime
+      } else if (lines[line].indexOf('グループ通話が終了しました。') !== -1) {
+        call.push([LastStartedCall, line_date.slice(0, -3) + ' ' + SentTime])
+        LastStartedCall = null
       } else {
-        text++
-        users[name]['contents']['text']++
+        statistics['text']++
+        user['contents']['text']++
+
         const regex_diary = new RegExp(/日記:\d?\d月\d?\d日/)
         if (regex_diary.test(lines[line])) {
           const regex_date_ja = new RegExp(/\d?\d月\d?\d日/)
@@ -97,27 +113,33 @@ function GetGata(got_text) {
               diary[diary_date] += lines[line + add_line] + '\n'
             }
           }
+
           $('#diary_tbody').append(
             `<tr><td>${diary_date}</td><td>${diary[diary_date]}</td></tr>`
           )
         }
       }
+
+      /*連続発言数*/
       if (lastUser == name) {
-        users[name]['continuously'][users[name]['continuously'].length - 1]++
+        user['continuously'][user['continuously'].length - 1]++
       } else {
-        users[name]['continuously'].push(1)
+        user['continuously'].push(1)
       }
       lastUser = name
+
     }
   }
-  //1000を超えた日をカウント
+
+  /*1000を超えた日をカウント*/
   const date_keys = Object.keys(date)
   for (let key in date_keys) {
     if (date[date_keys[key]] > 1000) {
       Over_1000_days++
     }
   }
-  //メンションのカウント
+
+  /*メンションのカウント*/
   for (const user in users) {
     if (Object.hasOwnProperty.call(users, user)) {
       const element = users[user]
@@ -126,12 +148,24 @@ function GetGata(got_text) {
     }
   }
 
+  /*合計通話時間*/
+  for (let i = 0; i < call.length; i++) {
+    const start = call[i][0]
+    const end = call[i][1]
+    const start_time = new Date(start)
+    const end_time = new Date(end)
+    console.log(start_time, end_time);
+    const diff = end_time.getTime() - start_time.getTime()
+    const diff_min = diff / (60 * 1000)
+    statistics['call'] += diff_min
+  }
+
   $('#status-text').text('完了')
   bar.value = 100
   $('#result, #search-box, #daily-data, #diary, #over_1000_days').show()
   $('#js-progressbar').hide()
   $('#result_title').text(
-    `総メッセージ数${message} | テキスト数${text} | 画像数${image} | 動画数${movie} | スタンプ数${stamp}`
+    `総メッセージ数${statistics['message']} | テキスト数${statistics['text']} | 画像数${statistics['image']} | 動画数${statistics['video']} | スタンプ数${statistics['stamp']} | 合計通話時間${statistics['call']}`
   )
   $('#over_1000_days').text(`1日に1000メッセージを超えた日: ${Over_1000_days}`)
   const keys = Object.keys(users)
@@ -139,24 +173,16 @@ function GetGata(got_text) {
     const user_name = keys[i]
     const user = users[keys[i]]
     $('#result_tbody').append(
-      `<tr><td>${user_name}</td><td>${user['message']}</td><td>${user['contents']['text']}</td><td>${user['contents']['image']}</td><td>${user['contents']['movie']}</td><td>${user['contents']['stamp']}</td><td>${user['mention']}</td><td>${Math.max.apply(Math, user['continuously'])}</td><td>${user['join']}</td></tr>`
+      `<tr><td>${user_name}</td><td>${user['message']}</td><td>${user['contents']['text']}</td><td>${user['contents']['image']}</td><td>${user['contents']['movie']}</td><td>${user['contents']['stamp']}</td><td>${user['mention']}</td><td>${user['contents']['call']}</td><td>${Math.max.apply(Math, user['continuously'])}</td><td>${user['join']}</td></tr>`
     )
   }
   $(document).ready(function () {
     $('#result_table').DataTable({
-      // 日本語表示
       language: {
         url: 'http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json',
       },
       displayLength: 50,
       order: [7, 'desc'],
-    })
-    $('#diary_table').DataTable({
-      // 日本語表示
-      language: {
-        url: 'http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json',
-      },
-      displayLength: 50,
     })
   })
   createDailyChart(date)
